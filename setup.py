04279525f6d20c9f82aba8770e1b41cec1c1c0d3
jirvingphd <james.irving.phd@gmail.com>
2020-03-26 13:06:45 -0400

    remove vers numbers

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['scikit-learn>=0.22.2','matplotlib>=3.2',]
+requirements = ['scikit-learn','matplotlib',]
 
 setup_requirements = [ ]
 
