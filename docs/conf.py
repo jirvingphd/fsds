d057d358421527e06cfa6c91b7754434e98fddb3
jirvingphd <james.irving.phd@gmail.com>
2020-03-26 21:05:37 -0400

    added run_apidoc

--- docs/conf.py
+++ docs/conf.py
@@ -160,3 +160,19 @@ texinfo_documents = [
 
 
 
+def run_apidoc(_):
+    #https://www.sphinx-doc.org/en/master/man/sphinx-apidoc.html
+	from sphinx.ext.apidoc import main
+	import os
+	import sys
+	sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+	cur_dir = os.path.abspath(os.path.dirname(__file__))
+	module = os.path.join(cur_dir,"..","fsds")
+	main(['-o', cur_dir, module,'-M','--force'])
+
+def setup(app):
+	app.connect('builder-inited', run_apidoc)
+
+
+
+
