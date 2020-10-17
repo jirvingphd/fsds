3057d6f1b715e0e22727ef02103a918f283c94e4
jirvingphd <james.irving.phd@gmail.com>
2020-10-16 20:52:10 -0400

    Bump version: 0.2.26 → 0.2.27

--- fsds/__init__.py
+++ fsds/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """James Irving"""
 __email__ = 'james.irving.phd@gmail.com'
-__version__ = '0.2.26'
+__version__ = '0.2.27'
 
 from .default import *
 
--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jirvingphd/fsds',
-    version='0.2.26',
+    version='0.2.27',
     zip_safe=False,
 )
