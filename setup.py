8b55a6457d17ca2770bfaf58eb10e620a2e33273
jirvingphd <james.irving.phd@gmail.com>
2021-09-08 13:29:39 -0400

    Bump version: 0.3.15 → 0.3.16

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jirvingphd/fsds',
-    version='0.3.15',
+    version='0.3.16',
     zip_safe=False,
 )
